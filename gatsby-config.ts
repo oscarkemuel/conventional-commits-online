65c94acde3b26d2232344b91e366c46c017869c1
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-04-22 20:03:41 -0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Conventional Commits Online`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
+export default config;
